a87c623a503d877910eb69b4c55f551655e6c5ef
Pham Tuan Hiep <hieppt.22bi13158@usth.edu.vn>
2024-03-07 09:31:06 +0700

    Practical 1

--- 1.student.mark.py
+++ 1.student.mark.py
@@ -1,74 +1,86 @@
-# student mark management
-students = []
-courses = []
-marks = {}
+class Student:
+    def __init__(self, stu_id, stu_name, stu_dob):
+        self.stu_id = stu_id
+        self.stu_name = stu_name
+        self.stu_dob = stu_dob
 
-def input_stu_inf():
-    num_students = int(input("Enter the number of students: "))
-    for i in range(num_students):
-        stu_id = input(f"Enter student {i+1}'s ID: ")
-        stu_name = input(f"Enter student {i+1}'s name: ")
-        stu_dob = input(f"Enter student {i+1}'s date of birth: ")
-        students.append((stu_id, stu_name, stu_dob))
+class Course:
+    def __init__(self, course_id, course_name):
+        self.course_id = course_id
+        self.course_name = course_name
 
-def input_courses():
-    print("--------------------------------")
-    num_courses = int(input("Enter the number of courses: "))
-    for j in range(num_courses):
-        course_id = input(f"Enter course {j+1}'s ID: ")
-        course_name = input(f"Enter course {j+1}'s name: ")
-        courses.append((course_id, course_name))
+class MarkManagement:
+    def __init__(self):
+        self.students = []
+        self.courses = []
+        self.marks = {}
 
-def input_marks():
-    print("--------------------------------")
-    print("Enter student's marks")
-    stu_id = input("Enter student ID: ")
-    course_id = input("Enter course ID: ")
-    mark = float(input("Enter mark for the student: "))
-    
-    if course_id not in marks:
-        marks[course_id] = {}
-    marks[course_id][stu_id] = mark
+    def input_stu_inf(self):
+        num_students = int(input("Enter the number of students: "))
+        for i in range(num_students):
+            stu_id = input(f"Enter student {i+1}'s ID: ")
+            stu_name = input(f"Enter student {i+1}'s name: ")
+            stu_dob = input(f"Enter student {i+1}'s date of birth: ")
+            self.students.append(Student(stu_id, stu_name, stu_dob))
 
-def list_courses():
-    print("--------------------------------")    
-    print("List of courses:")
-    for course in courses:
-        print(f"Course ID: {course[0]}, Course Name: {course[1]}")
+    def input_courses(self):
+        print("--------------------------------")
+        num_courses = int(input("Enter the number of courses: "))
+        for j in range(num_courses):
+            course_id = input(f"Enter course {j+1}'s ID: ")
+            course_name = input(f"Enter course {j+1}'s name: ")
+            self.courses.append(Course(course_id, course_name))
 
-def list_stu():
-    print("List of students:")
-    for student in students:
-        print(f"Student ID: {student[0]}, Student Name: {student[1]}, DoB: {student[2]}")
+    def input_marks(self):
+        print("--------------------------------")
+        print("Enter student's marks")
+        stu_id = input("Enter student ID: ")
+        course_id = input("Enter course ID: ")
+        mark = float(input("Enter mark for the student: "))
 
-def show_stu_marks():
-    print("--------------------------------")
-    course_id = input("Enter course ID to show marks: ")
-    if course_id in marks:
-        print(f"Student marks for course {course_id}:")
-        for stu_id, mark in marks[course_id].items():
-            print(f"Student ID: {stu_id}, Mark: {mark}")
-    else:
-        print(f"No marks are found for course {course_id}")
+        if course_id not in self.marks:
+            self.marks[course_id] = {}
+        self.marks[course_id][stu_id] = mark
 
-input_stu_inf()
-input_courses()
-list_courses()
-list_stu()
+    def list_courses(self):
+        print("--------------------------------")
+        print("List of courses:")
+        for course in self.courses:
+            print(f"Course ID: {course.course_id}, Course Name: {course.course_name}")
+
+    def list_students(self):
+        print("List of students:")
+        for student in self.students:
+            print(f"Student ID: {student.stu_id}, Student Name: {student.stu_name}, DoB: {student.stu_dob}")
+
+    def show_stu_marks(self):
+        print("--------------------------------")
+        course_id = input("Enter course ID to show marks: ")
+        if course_id in self.marks:
+            print(f"Student marks for course {course_id}:")
+            for stu_id, mark in self.marks[course_id].items():
+                print(f"Student ID: {stu_id}, Mark: {mark}")
+        else:
+            print(f"No marks are found for course {course_id}")
+    print("-----------------------------")
+
+m = MarkManagement()
+m.input_stu_inf()
+m.input_courses()
+m.list_courses()
+m.list_students()
 
 while True:
     print("\nPress 1 to input marks")
     print("Press 2 to show student marks")
     print("Press 3 to quit")
-    
+
     choice = input("Enter 1 to 3: ")
     if choice == '1':
-        input_marks()
+        m.input_marks()
     elif choice == '2':
-        show_stu_marks()
+        m.show_stu_marks()
     elif choice == '3':
         break
     else:
-        print("Fail number")
-
-                                                                                                                         
+        print("Invalid choice")
